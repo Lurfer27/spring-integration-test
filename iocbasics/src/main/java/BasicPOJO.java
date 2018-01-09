334009b1e037610e5eeb227737976a17072b0aad
Lurfer27 <lee.p.richards@gmail.com>
2018-01-09 10:39:17 +0000

    Add iocbasics

---
+++ iocbasics/src/main/java/BasicPOJO.java
@@ -0,0 +1,29 @@
+public class BasicPOJO {
+
+    public String name;
+    public ColorEnum color;
+
+    public BasicPOJO() {
+    }
+
+    public BasicPOJO(String name, ColorEnum color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ColorEnum getColor() {
+        return color;
+    }
+
+    public void setColor(ColorEnum color) {
+        this.color = color;
+    }
+}
